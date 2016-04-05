c34d0aec449e7f89346ad628cfcc18fa6caec378
Pradeep <krnprdp@gmail.com>
2016-04-05 18:54:39 -0400

    Add fib_ultimate -> O(1) Space and O(n) time solution using DP

--- src/com/pradeep/dynamicprogramming/Fibonacci.java
+++ src/com/pradeep/dynamicprogramming/Fibonacci.java
@@ -8,17 +8,35 @@ package com.pradeep.dynamicprogramming;
 public class Fibonacci {
 
 	public static int fib_dp(int n) {
-		int fib[] = new int[n+1];
+		int fib[] = new int[n + 1];
 		fib[0] = 0;
 		fib[1] = 1;
 
-		for (int i = 2; i <= n; i++)
+		for (int i = 2; i <= n; i++) {
+			System.out.print("\t" + " fib(" + i + ") ->\n");
 			fib[i] = fib[i - 1] + fib[i - 2];
-
+			System.out.println("fib(" + i + ") = " + fib[i]);
+		}
 		return fib[n];
 	}
-	
-	public static void main(String[] args){
-		System.out.println(fib_dp(8));
+
+	public static long fib_ultimate(int n) {
+		int back1 = 0;
+		int back2 = 1;
+		int next = 0;
+		for (int i = 2; i <= n; i++) {
+			next = back1 + back2;
+			back1 = back2;
+			back2 = next;
+		}
+		return next;
+	}
+
+	public static void main(String[] args) {
+		
+		int i = 9;
+		
+		System.out.println(fib_dp(i));
+		System.out.println("Using fib_ultimate: "+fib_ultimate(i));
 	}
 }

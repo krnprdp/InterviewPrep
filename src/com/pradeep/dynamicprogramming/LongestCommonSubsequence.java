a0bc9d6f1a6b1579e7575994e348e08c027e9934
Pradeep <krnprdp@gmail.com>
2016-04-05 21:41:29 -0400

    Add Longest Common Subsequence solution

---
+++ src/com/pradeep/dynamicprogramming/LongestCommonSubsequence.java
@@ -0,0 +1,41 @@
+package com.pradeep.dynamicprogramming;
+
+public class LongestCommonSubsequence {
+	public static void LCS(char[] input1, char[] input2) {
+
+		int[][] T = new int[input1.length + 1][input2.length + 1];
+
+		for (int i = 0; i < input1.length; i++) {
+			T[i][0] = 0;
+		}
+
+		for (int i = 0; i < input2.length; i++) {
+			T[0][i] = 0;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i < input1.length; i++) {
+			for (int j = 1; j < input2.length; j++) {
+				if (input1[i] == input2[j]) {
+					T[i][j] = T[i - 1][j - 1] + 1;
+
+					if (sb.length() != 0)
+						sb.append(", ");
+					sb.append(input1[i]);
+					
+				} else{
+					T[i][j] = Math.max(T[i][j-1], T[i-1][j]);
+				}
+			}
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static void main(String[] args) {
+		String input1 = "abcdaf";
+		String input2 = "acbcf";
+
+		LCS(input1.toCharArray(), input2.toCharArray());
+	}
+}

6d3b5bbf415fe5935faf5e83657deef4e6fd778f
Pradeep <krnprdp@gmail.com>
2016-04-21 20:44:37 -0400

    Insertion Sort Implementation

--- src/com/pradeep/sortingalgorithms/InsertionSort.java
+++ src/com/pradeep/sortingalgorithms/InsertionSort.java
@@ -2,4 +2,30 @@ package com.pradeep.sortingalgorithms;
 
 public class InsertionSort {
 
+	public static void main(String[] args) {
+
+		int[] arr = { 53, 12, 35, 21, 59, 15 };
+		for (int i : arr)
+			System.out.print(i + " ");
+		
+		System.out.println();
+		sort(arr,arr.length);
+		for (int i : arr)
+			System.out.print(i + " ");
+		
+	}
+
+	public static void sort(int[] arr,int n){
+		
+		for (int j = 1; j < n; j++){
+			int key = arr[j];
+			int i = j - 1;
+			
+			while (i >= 0 && arr[i] > key){
+				arr[i + 1] = arr[i];
+				i--;
+			}
+			arr[i+1] = key;
+		}
+	}
 }

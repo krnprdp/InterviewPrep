3779dae4c4ebf29501489a66803f57c211c27d4b
Pradeep <krnprdp@gmail.com>
2016-05-04 20:26:12 -0400

    queue mods

--- src/com/pradeep/datastructures/Queue.java
+++ src/com/pradeep/datastructures/Queue.java
@@ -3,8 +3,8 @@ package com.pradeep.datastructures;
 public class Queue {
 
 	private int[] arr;			// Array for holding the Queue elements
-	private int front;			// Front of queue
-	private int size;			// Number of elements at any given time
+	private int front = 0;			// Front of queue
+	private int size = 0;			// Number of elements at any given time
 	
 	public Queue(int capacity){
 		arr = new int[capacity];
